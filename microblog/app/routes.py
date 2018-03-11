f9550810d3a0204618d173388b9bede162caba45
Sczaleg <igor.strzalka@gmail.com>
2018-03-11 01:16:47 +0100

    Kurwa jarek chce miec date to bedzie mial date

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request, jsonify
+from flask import render_template, request, jsonify, abort
 from bigchaindb_driver import BigchainDB
 from math import sin, cos, sqrt, atan2, radians
 from bigchaindb_driver.crypto import generate_keypair
@@ -11,47 +11,28 @@ import re
 import operator
 
 def calc_distance(lat1, lon1, lat2, lon2):
-    # returns in distance in kms
+    # returns in distance in metres
     R = 6373.0
-    lat1 = radians(lat1)
-    lat2 = radians(lat2)
-    lon1 = radians(lon1)
-    lon2 = radians(lon2)
-    dif_lon = lon2-lon1
-    dif_lat = lat2-lat1
-    a = sin(dif_lat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dif_lon / 2) ** 2
+    dif_lon = radians(lon2-lon1)
+    dif_lat = radians(lat2-lat1)
+    a = sin(dif_lat / 2) ** 2 + cos(radians(lat1)) * cos(radians(lat2)) * sin(dif_lon / 2) ** 2
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    return R * c
-
-
+    return R * c * 1000
 
 @app.route('/')
 @app.route('/index')
 
-
 def index():
-    user = {'username':'igorek'}
-    list = [
-        {
-            'name': 'chuj',
-            'link': 'xddx'
-        },
-        {
-            'name': 'chuj2',
-            'link': 'xd'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, post=list)
+    abort(404)
 
 @app.route('/api/messages/', methods=['GET', 'POST'])
 def retrieve_messages():
     bdb = BigchainDB('http://80.211.240.79:59984')
     data = bdb.assets.get(search="message")
-    # chuj = {item for item in data}
     if request.method == 'GET':
         if len(request.args) == 0:
             return jsonify(data)
-        else:# username = request.args.get('username')
+        else:
             long = float(request.args.get('lon'))
             lati = float(request.args.get('lat'))
             radius = float(request.args.get('radius'))
@@ -69,10 +50,7 @@ def retrieve_messages():
             asset={'data': entry, 'metadata': {"type": "message"}})
         signed_tx = bdb.transactions.fulfill(tx, private_keys=keypair.private_key)
         x = bdb.transactions.send(signed_tx)
-        return jsonify(x['asset'])
-        # for item in data:
-        #     if (lati, long) == (item['data']['lat'], item['data']['lon']):
-        #         if (message, auth) == (item['data']['message'], item['data']['author']):
-        #             return jsonify(item)
-            # render_template('assets.html', data = data)
+        y = x['asset']
+        y['id'] = x['id']
+        return jsonify(y)
 
